666620a594d6e6e3dce74debe302a484a6fe23ce
Gavin <Gavin@10.80.140.201>
2018-10-31 11:03:23 -0400

    Added a println

---
+++ A5/src/gitPractice/main.java
@@ -0,0 +1,10 @@
+package gitPractice;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is just practice");
+	}
+
+}
